745e03b01d6ffd5b544f00a868109c23afa23b07
sanjanathammayya <53755440+sanjanathammayya@users.noreply.github.com>
2020-05-20 19:19:53 +0530

    Update ShortestPalindrome.java

--- ShortestPalindrome.java
+++ ShortestPalindrome.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public static String ShortestPalindrome {
+ public static String shortestPalindrome(String str) {
+   int x=0;
+   int y=str.length()-1;
+   while n(y>=0){
+     if(str.charAt(x)==str.charAt(y)){
+       x++;
+     }
+     y--;
+   }
+   if(x==str.length())
+     return str;
+   String suffix = str.substring(x);
+   String prefix = new StringBuilder(suffix).reverse()..toString();
+   String mid = shortestPalindrome(str.substring(0,x));
+   return prefix+mid+suffix;
+ }
+  public static void main(string[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("enter a string to find out shortest palindrome of "+str+" is "+shortestPalindrome(str));
+  }
+}
+    
